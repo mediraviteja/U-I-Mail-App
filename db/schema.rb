b9b72f3aeb24314d703d169ae416500d2795a6a0
Ravi Theja <raviteja@raviteja-PI945GCM.(none)>
2014-04-21 00:24:27 +0530

    added new fields in sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140406162359) do
+ActiveRecord::Schema.define(version: 20140420175825) do
 
   create_table "projects", force: true do |t|
     t.string   "First"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20140406162359) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_name"
+    t.integer  "phone_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
